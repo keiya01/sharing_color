ae6be3cfe9d21e1aef16732fb5ed2d2d4cec5d46
keiya01 <keiya.s.0210@gmail.com>
2019-03-24 17:06:31 +0900

    Fix button.stories.js

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -6,6 +6,3 @@ storiesOf('Button', module)
   .add('with text', () => (
     <Button>Hello Button</Button>
   ))
-  .add('with emoji', () => (
-    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-  ));   
