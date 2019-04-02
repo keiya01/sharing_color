7b0ebd81d374f22d69040aea4f50ee46196eef10
keiya01 <keiya.s.0210@gmail.com>
2019-04-02 22:44:44 +0900

    Add color input function

--- src/ColorForm.tsx
+++ src/ColorForm.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import styled, { css } from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const { useState } = React;
+const { useState, useReducer } = React;
 
 const Form = styled.div`
   width: 500px;
@@ -21,13 +21,13 @@ const InputContainer = styled.div`
 
 const Icon = styled.div`
   position: absolute;
-  top: 3px;
-  left: 70px;
+  top: 6px;
+  left: 138px;
   color: #aaa;
 `;
 
 const ColorInput = styled.input.attrs({ type: "text" })`
-  width: 270px;
+  width: 130px;
   padding: 5px;
   padding-left: 30px;
   font-size: 16px;
@@ -37,13 +37,15 @@ const ColorInput = styled.input.attrs({ type: "text" })`
   border-bottom: 1px solid #FF684A;
   border-top-left-radius: 5px;
   border-bottom-left-radius: 5px;
+  letter-spacing: 0.15em;
+  color: #555;
   &:focus {
     outline: none;
   }
 `;
 
 const Button = styled.button`
-  padding: 6px;
+  padding: 8px;
   font-size: 15px;
   background-color: #FF684A;
   color: #fff;
@@ -55,26 +57,90 @@ const Button = styled.button`
   &:hover {
     background-color: #FF937E;
   }
+  &:focus {
+    outline: none;
+  }
 `;
 
+interface Color {
+  color: string,
+}
+interface State {
+  colors: Color[],
+}
+const initialState: State = {
+  colors: [],
+};
+
+interface Action {
+  type: string,
+  color: Color,
+}
+
+let colorId = 0;
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "ADD_COLOR": {
+      colorId++;
+      const color = {
+        id: colorId,
+        ...action.color,
+      }
+      return {
+        ...state,
+        colors: [
+          ...state.colors,
+          color,
+        ]
+      };
+    }
+    default: return state;
+  }
+};
+
 const ColorForm: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [text, changeText] = useState("");
 
   const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
-    changeText(event.target.value);
+    const value = event.target.value;
+    if (value.length > 7) {
+      return;
+    }
+    changeText(value);
   };
 
-  // \uhhhh
+  const handleOnClick = () => {
+    const isHexadeciam = text.match(/^#?[0-9|a-f]{6}/i);
+    if (!isHexadeciam) {
+      return;
+    }
+
+    const hasHashtag = text.startsWith("#");
+    let color = text;
+    if (!hasHashtag) {
+      color = `#${color}`;
+    }
+
+    if(color.length > 7) {
+      return;
+    }
+
+    dispatch({ type: "ADD_COLOR", color: { color } });
+  }
 
   return (
     <Form>
       <InputContainer>
-          <Icon>
-            <FontAwesomeIcon icon={"hashtag"} />
-          </Icon>
-          <ColorInput value={text} onChange={handleChangeText} />
-        <Button>register</Button>
+        <Icon>
+          <FontAwesomeIcon icon={"hashtag"} />
+        </Icon>
+        <ColorInput value={text} onChange={handleChangeText} />
+        <Button onClick={handleOnClick}>register</Button>
       </InputContainer>
+      {state.colors.map(item => (
+        <p>{item.color}</p>
+      ))}
     </Form>
   );
 }
